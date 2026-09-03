f6ec9005d1e60e9063439a468b4f1e5a726746c6
Dawson Blackhouse <dcblackhouse@gmail.com>
2026-09-03 17:37:18 -0400

    feat: adds annual pricing to pricing page

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -41,6 +41,8 @@ export const auth = betterAuth({
           {
             name: "premium",
             priceId: process.env.STRIPE_PREMIUM_PRICE_ID || "",
+            annualDiscountPriceId:
+              process.env.STRIPE_PREMIUM_ANNUAL_PRICE_ID || "",
           },
         ],
       },
